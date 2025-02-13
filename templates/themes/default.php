52de7e4cc609d39c5e290e399eb2081f72e77978
sumonazma <engr.sumonazma@gmail.com>
2025-02-13 13:29:29 +0600

    details page add

--- templates/themes/default.php
+++ templates/themes/default.php
@@ -1,1 +1,10 @@
 <?php
+	/*
+   * @Author 		engr.sumonazma@gmail.com
+   * Copyright: 	mage-people.com
+   */
+	if ( ! defined( 'ABSPATH' ) ) {
+		die;
+	} // Cannot access pages directly.
+	$post_id = get_the_id();
+	echo $post_id;
