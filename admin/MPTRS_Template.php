4a0b003fc98cf29459d851aca58ee3fd05d63c7b
MagePeople,Inc <48548664+magepeopleteam@users.noreply.github.com>
2025-07-15 08:35:36 +0600

    Update MPTRS_Template.php

--- admin/MPTRS_Template.php
+++ admin/MPTRS_Template.php
@@ -276,6 +276,8 @@ if (!class_exists('MPTRS_Template')) {
             ?>
             <?php if (!empty($existing_menus)) { ?>
 
+<div class="mptrs-top-menu-left">
+	
                 <h4><?php esc_html_e('Menu', 'tablely'); ?> (<?php echo esc_html(count($existing_menus)); ?>)</h4>
                 <div class="mptrs-category-container">
                     <input type="hidden" id="mptrs_location_autocomplete" value="<?php echo esc_attr( $enable_location_autocomplete );?>">
@@ -293,6 +295,9 @@ if (!class_exists('MPTRS_Template')) {
                             ?>
                         <?php } ?>
                     <?php } ?>
+		
+		</div>
+			
                     <div class="mptrs-more-button">...</div>
 
                 </div>
